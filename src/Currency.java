b284c54c61bf00c9f2584e1555cc2e652f753584
Beau Fontenot <104475813+bfontenot23@users.noreply.github.com>
2023-10-23 17:36:20 -0500

    Finished Project

---
+++ src/Currency.java
@@ -0,0 +1,46 @@
+public class Currency {
+
+    private final String name;
+    private final double exchangeRate;
+    private double amount;
+
+    public Currency(String name, double exchangeRate, double amount)
+    {
+        this.name = name;
+        this.exchangeRate = exchangeRate;
+        this.amount = amount;
+    }
+
+    public Currency()
+    {
+        this.name = "";
+        this.exchangeRate = 0.0;
+        this.amount =0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRate() {
+        return exchangeRate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getCreditValue()
+    {
+        return (this.getRate()*this.getAmount());
+    }
+
+    public double checkExchange(Currency foreign)
+    {
+        return ((this.getCreditValue()/foreign.getRate()));
+    }
+}
---
+++ src/CurrencyExchange.java
@@ -0,0 +1,20 @@
+public class CurrencyExchange {
+
+    static final double DOLLARS_TO_CREDITS = 53.4;
+    static final double BLEGHTHERS_TO_CREDITS = 2.7;
+    static final double SMORGOS_TO_CREDITS = 1276.32;
+    public static void main(String[] args)
+    {
+        Currency dollars,bleghthers,smorgos;
+        dollars = new Currency("Dollar", DOLLARS_TO_CREDITS, 27);
+        bleghthers = new Currency("Bleghther", BLEGHTHERS_TO_CREDITS, 2931);
+        smorgos = new Currency("Smorgo", SMORGOS_TO_CREDITS, 3);
+
+        System.out.printf("[*] %.2f %ss are worth %f credits\n",smorgos.getAmount(),smorgos.getName(),smorgos.getCreditValue());
+        System.out.printf("[*] %s\n",bleghthers.getName());
+        System.out.printf("[*] With %.2f %ss, you can buy %.4f %ss\n",dollars.getAmount(),dollars.getName(),dollars.checkExchange(smorgos),smorgos.getName());
+        System.out.printf("[*] With %.2f %ss, you can buy %.2f %ss\n",bleghthers.getAmount(),bleghthers.getName(),bleghthers.checkExchange(dollars),dollars.getName());
+        System.out.printf("[*] %.2f %ss, %.2f %ss, and %.4f %ss are worth: %f Credits\n",dollars.getAmount(),dollars.getName(),bleghthers.getAmount(),bleghthers.getName(),smorgos.getAmount(),smorgos.getName(),dollars.getCreditValue()+bleghthers.getCreditValue()+smorgos.getCreditValue());
+        System.out.printf("[*] If we converted everything to dollars, we would have %.2f %ss\n",bleghthers.checkExchange(dollars)+smorgos.checkExchange(dollars)+dollars.getAmount(),dollars.getName());
+    }
+}
